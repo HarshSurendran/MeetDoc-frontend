5b8240c41bd4f204ef748e88842722984e77050b
Harsh Surendran <harshsurendran@gmail.com>
2025-01-08 11:53:01 +0530

    ADD - Create Verification component for managing doctor verification requests

---
+++ src/components/admin/Verification.tsx
@@ -0,0 +1,99 @@
+import { Card } from '@mui/material'
+import React from 'react'
+import { FormData } from '../doctor/steps/types';
+import { getVerificationRequests } from '@/services/admin/admin';
+import errorHandler from '@/utils/errorHandler';
+import { useNavigate } from 'react-router-dom';
+
+const Verification = () => {
+    const [pendingRequests, setpendingRequests] = React.useState<FormData[]>([]);
+    const [approvedRequests, setapprovedRequests] = React.useState<FormData[]>([]);
+    const navigate = useNavigate();
+
+    React.useEffect(() => {
+        getRequests();
+    }, []);
+
+    const getRequests = async () => {
+        try {
+            const res = await getVerificationRequests();
+            const pendingData = res.data.filter((doc: FormData) => doc.isVerified === false);
+            setpendingRequests(pendingData);
+            const approvedData = res.data.filter((doc: FormData) => doc.isVerified === true);
+            setapprovedRequests(approvedData);
+        } catch (error) {
+            errorHandler(error);
+        }
+    };
+  
+
+    return (
+        <div className="container mx-auto p-4 max-w-7xl">
+            <Card className="overflow-hidden mb-6">
+                <div className="p-6 border-b">
+                    <h2 className="text-lg font-semibold">Pending Verification</h2>
+                </div>
+                <div className="overflow-x-auto">
+                    <table className="w-full text-center">
+                        <thead className="bg-gray-50">
+                            <tr>
+                                <th className="px-6 py-3  text-sm font-medium text-gray-500">Name</th>
+                                <th className="px-6 py-3  text-sm font-medium text-gray-500">Email</th>
+                                <th className="px-6 py-3  text-sm font-medium text-gray-500">Gender</th>
+                                <th className="px-6 py-3  text-sm font-medium text-gray-500">Age</th>
+                                <th className="px-6 py-3  text-sm font-medium text-gray-500">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody className="divide-y divide-gray-200">
+                            {pendingRequests.map((doctor) => (
+                                <tr key={doctor.doctorId} >
+                                    <td className="px-6 py-4 text-sm text-gray-900">{doctor?.personalDetails.name}</td>
+                                    <td className="px-6 py-4 text-sm text-gray-500">{doctor?.personalDetails.email}</td>
+                                    <td className="px-6 py-4 text-sm text-gray-500">{doctor?.personalDetails.gender}</td>
+                                    <td className="px-6 py-4 text-sm text-gray-500">{doctor?.personalDetails.age}</td>
+                                    <td className="px-6 py-4 text-sm">
+                                        <button className='px-3 py-1 rounded-md bg-blue-50 text-blue-600  hover:bg-blue-100' onClick={() => navigate(`/admin/doctordetails/${doctor.doctorId}`)}>View Details</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </Card>
+
+            <Card className="overflow-hidden mb-6">
+                <div className="p-6 border-b">
+                    <h2 className="text-lg font-semibold">Approved Verification</h2>
+                </div>
+                <div className="overflow-x-auto">
+                    <table className="w-full text-center">
+                        <thead className="bg-gray-50">
+                            <tr>
+                                <th className="px-6 py-3  text-sm font-medium text-gray-500">Name</th>
+                                <th className="px-6 py-3  text-sm font-medium text-gray-500">Email</th>
+                                <th className="px-6 py-3  text-sm font-medium text-gray-500">Gender</th>
+                                <th className="px-6 py-3  text-sm font-medium text-gray-500">Age</th>
+                                <th className="px-6 py-3  text-sm font-medium text-gray-500">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody className="divide-y divide-gray-200">
+                            {approvedRequests.map((doctor) => (
+                                <tr key={doctor.doctorId} >
+                                    <td className="px-6 py-4 text-sm text-gray-900">{doctor?.personalDetails.name}</td>
+                                    <td className="px-6 py-4 text-sm text-gray-500">{doctor?.personalDetails.email}</td>
+                                    <td className="px-6 py-4 text-sm text-gray-500">{doctor?.personalDetails.gender}</td>
+                                    <td className="px-6 py-4 text-sm text-gray-500">{doctor?.personalDetails.age}</td>
+                                    <td className="px-6 py-4 text-sm">
+                                        <button className='px-3 py-1 rounded-md bg-blue-50 text-blue-600  hover:bg-blue-100' onClick={() => navigate(`/admin/doctordetails/${doctor.doctorId}`)}>View Details</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </Card>
+            </div>
+    );
+}
+
+export default Verification
--- src/routes/AdminRouter.tsx
+++ src/routes/AdminRouter.tsx
@@ -7,6 +7,7 @@ import AdminMainContent from "@/components/admin/AdminMainContent";
 import UserManagementTable from "@/components/admin/UserManagement";
 import UserDetailView from "@/components/admin/UserDetailView";
 import DoctorVerificationPage from "@/Pages/admin/DoctorVerificationPage";
+import Verification from "@/components/admin/Verification";
 
 const AdminRouter = () => {
   return (
@@ -15,6 +16,7 @@ const AdminRouter = () => {
       <Route path="/" element={<PrivateRoute component={AdminLayout} />} >
         <Route index path="dashboard" element={<AdminMainContent />} /> 
         <Route path="doctors" element={<h1>Doctors</h1>} />
+        <Route path="verification" element={<Verification/>} />
         <Route path="doctorDetails/:id" element={<DoctorVerificationPage/>} />
         <Route path="users" element={<UserManagementTable />} />
         <Route path="users/:id" element={<UserDetailView />} />
